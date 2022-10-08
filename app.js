5c315108011ec6ffddb7ec3192966648f3df01e0
Shashank <srkota1951@gmail.com>
2022-10-08 19:55:53 +0530

    working  app.js and styles

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const express = require('express');
 const methodOverride = require('method-override')
 const app = express();
 const path = require('path');
-const Joi = require('joi');  
-const bcrypt = require('bcrypt');
-const session = require('express-session')
-const cookieParser = require('cookie-parser');
+// const Joi = require('joi');  
+// const bcrypt = require('bcrypt');
+// const session = require('express-session')
+// const cookieParser = require('cookie-parser');
 
 //Database imports
 const mongoose = require('mongoose');
-const Product = require('./models/products');
-const User = require('./models/user');
 
-const wrapAsync = require('./utils/wrapAsync');
-const ExpressError = require('./utils/ExpressError');
-const { productSchema, userSchema } = require('./schemas.js');
+// const wrapAsync = require('./utils/wrapAsync');
+// const ExpressError = require('./utils/ExpressError');
+// const { productSchema, userSchema } = require('./schemas.js');
 
 // mongoose.connect('mongodb://localhost:27017/auctionSystem')
 //     .then(() => {
@@ -28,7 +26,7 @@ const { productSchema, userSchema } = require('./schemas.js');
 app.use(express.urlencoded({ extended: true }))
 
 // To parse incoming JSON in POST request body:
-app.use(express.json())
+// app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
@@ -47,7 +45,7 @@ app.set('view engine', 'ejs')
 
 
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render('index');
 })
 
@@ -60,10 +58,10 @@ app.listen(5000, () => {
 })
 
 // To handle all the remaining path errors
-app.all('*', (req, res, next) => {
+// app.all('*', (req, res, next) => {
     
-    next(new ExpressError('Page not found', 404));
-})
+//     next(new ExpressError('Page not found', 404));
+// })
 
 app.use((err, req, res, next) => {
     const {statusCode = 500, message = 'Something went wrong'}  = err;
